57da380537f6d8f15af8750074306421de04111b
danlaptev <danlaptev@yandex-team.ru>
2023-06-10 17:10:44 +0300

    выпилил лишнее

--- src/routes/checkout/+page.js
+++ src/routes/checkout/+page.js
@@ -1,13 +1,7 @@
 import { error } from '@sveltejs/kit';
-import { handleFetchCart } from '$lib/handlers/cart';
 
 export async function load() {
-	try {
-		await handleFetchCart();
-		return {
-			status: 200
-		};
-	} catch (err) {
-		throw error(500);
-	}
+	return {
+		status: 200
+	};
 }
